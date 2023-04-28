1e3b33fe1174d2ec2dc42fa91c1e2eef23520416
Nino <ninodero@buffalo.edu>
2023-04-28 13:52:40 -0400

    added a newline in Readme

---
+++ flask_project/manage.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return "Welcome to my Flask app!"
+
+@app.route("/greetings/christmas")
+def christmas():
+	return "Merry Christmas!"
+
+@app.route("/greetings/newyear")
+def newyear():
+	return "Happy New Year!"
+
+if __name__ == "__main__":
+	app.run()
